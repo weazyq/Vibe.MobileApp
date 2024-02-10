7f7ae2a71c2fa036b91052d750d0fd851646ca6f
Vladislav Kasiyan <weazy@internet.ru>
2024-02-10 14:10:44 +0300

    Добавление страницы ввода отправленного кода на телефон

---
+++ apps/login/pages/checkSmsPage.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import { Alert, Text, View } from "react-native"
+import PhoneCodeInput from "../../../components/inputs/phoneCodeInput/phoneCodeInput"
+import { ClientProvider } from "../../../domain/clients/clientProvider"
+import { containerStyles, textStyles } from "../../../styles/styles"
+import { useLoginPage } from "../LoginContext"
+
+function CheckSmsPage() {
+    const { clientBlank } = useLoginPage()
+
+    const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    const phoneCodePattern: RegExp = /^\d{4}$/
+
+    useEffect(() => {
+        sendSms()
+    }, [])
+
+    async function sendSms() {
+        const response = await ClientProvider.sendSms(clientBlank.phone)
+        if (response.isSuccess) Alert.alert('Отправлен код')
+    }
+
+    async function handlePhoneCodeChanged(phoneCode: string) {
+        if (!phoneCodePattern.test(phoneCode)) return
+
+        const response = await ClientProvider.checkSms(clientBlank.phone, phoneCode)
+        if (!response.isSuccess) return setErrorMessage(response.errors[0])
+    }
+
+    return (
+        <View style={[containerStyles.flexCenter, containerStyles.fullHeight, { gap: 10 }]}>
+            <Text style={[textStyles.header2, textStyles.textCenter, { marginBottom: 20 }]}>
+                Введи код из SMS
+            </Text>
+            <Text style={[textStyles.header4, textStyles.textCenter]}>
+                Код придёт в течении минуты на номер:
+            </Text>
+            <Text style={[textStyles.primaryText, textStyles.textCenter]}>
+                {clientBlank.phone}
+            </Text>
+            <PhoneCodeInput
+                onChange={handlePhoneCodeChanged}
+            />
+            {errorMessage &&
+                <Text>
+                    {errorMessage}
+                </Text>
+            }
+        </View>
+    )
+}
+
+export default CheckSmsPage
--- domain/clients/clientProvider.ts
+++ domain/clients/clientProvider.ts
@@ -1,28 +1,31 @@
+import axios from "axios";
 import Constants from "../../constants/constants";
+import Result from "../../tools/result";
 import { ClientBlank } from "./clientBlank";
 
 export class ClientProvider {
-    static async sendSms(phone: string): Promise<string> {
-        const response = await fetch(`${Constants.serverUrl}/?phoneNumber=${phone}`, {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json'
-            }
-        }).then(response => response.json())
+    static async sendSms(phoneNumber: string): Promise<Result> {
+        const response = await axios.get(`${Constants.serverUrl}/SendSms`, {
+            params: {
+                phoneNumber: phoneNumber
+            },
+            ...Constants.axiosConfig
+        })
 
-        return response
+        if (!response.data.isSuccess) return Result.fail(response.data.errors[0])
+        return Result.success(null)
+    }
+
+    static async checkSms(phoneNumber: string, code: string): Promise<Result> {
+        const response = await axios.post(`${Constants.serverUrl}/CheckSms`, { phoneNumber, code }, Constants.axiosConfig)
+
+        if (!response.data.isSuccess) return Result.fail(response.data.errors[0])
+        return Result.success(null)
     }
 
     static async registerClient(blank: ClientBlank): Promise<string> {
-        const response = await fetch(`${Constants.serverUrl}/RegisterClient`, {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(blank)
-        }).then(response => response.json())
+        const response = await axios.post(`${Constants.serverUrl}/RegisterClient`, blank, Constants.axiosConfig)
 
-        return response
+        return JSON.stringify(response)
     }
 }

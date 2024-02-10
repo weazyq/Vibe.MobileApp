c726e2a7fca618e011b26fcadf1e5c074296f6ff
Vladislav Kasiyan <weazy@internet.ru>
2024-02-10 14:06:12 +0300

    Добавление стилей

---
+++ styles/styles.ts
@@ -0,0 +1,93 @@
+import { StyleSheet } from 'react-native'
+
+export class Colors {
+    static primary = {
+        light: "#53AFE3",
+        main: "#299CDD",
+        dark: "#1C6D9A",
+        contrastText: "#fff"
+    }
+
+    static secondary = {
+        light: "#8F97ED",
+        main: "#737DE9",
+        dark: "#5057A3",
+        contrastText: "#000"
+    }
+
+    static inputBorder = '#767676'
+    static disabled = '#BCBCBC'
+    static disabledText = '#A1A1A1'
+}
+
+export const textStyles = StyleSheet.create({
+    header1: {
+        fontSize: 36,
+        fontWeight: 'bold'
+    },
+    header2: {
+        fontSize: 28,
+        fontWeight: 'bold'
+    },
+    header3: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    header4: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+
+    paragraph: {
+        fontSize: 10
+    },
+
+    primaryText: {
+        color: Colors.primary.light
+    },
+
+    secondaryText: {
+        color: Colors.disabledText
+    },
+
+    textCenter: {
+        textAlign: 'center'
+    },
+})
+
+export const containerStyles = StyleSheet.create({
+    flexCenter: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    flexSpaceBetween: {
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    fullHeight: {
+        height: '100%'
+    },
+    fullWidth: {
+        width: '100%'
+    }
+})
+
+export const inputStyles = StyleSheet.create({
+    default: {
+        padding: 10,
+        borderRadius: 10,
+        fontSize: 16,
+        borderColor: Colors.inputBorder,
+        borderWidth: 1
+    },
+
+    blur: {
+        borderColor: Colors.primary.light
+    },
+
+    inputTitle: {
+        ...textStyles.header4,
+        marginBottom: 10
+    }
+})

2b72a7e25b07cd329b9af70bc2ce743d71d54f54
Vladislav Kasiyan <weazy@internet.ru>
2024-02-11 17:12:39 +0300

    Исправление стилей корня приложения, адаптация стилей других страниц

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import AuthProvider, { useAuth } from './AuthProvider';
 import LoginProvider from './apps/login/LoginContext';
 import LoginScreen from './apps/login/loginScreen';
+import RentalProvider from './apps/rental/rentalContext';
+import RentalScreen from './apps/rental/rentalScreen';
 
 export default function App() {
   return (
@@ -14,14 +16,23 @@ export default function App() {
   function AppContent() {
     const { isAuthenticated } = useAuth()
 
+    const styles = StyleSheet.create({
+      container: {
+        width: "100%",
+        height: "100%",
+        paddingTop: 50
+      }
+    })
+
     return (
       <>
         <StatusBar backgroundColor='#fff' />
-        <SafeAreaView style={{ paddingTop: 30, backgroundColor: '#fff' }}>
+        <SafeAreaView style={styles.container}>
           {
             isAuthenticated
-              ? <>
-              </>
+              ? <RentalProvider>
+                <RentalScreen />
+              </RentalProvider>
               : <LoginProvider>
                 <LoginScreen />
               </LoginProvider>
--- apps/login/loginScreen.tsx
+++ apps/login/loginScreen.tsx
@@ -1,27 +1,13 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
 import { LoginPageType, useLoginPage } from './LoginContext';
 import CheckSmsPage from './pages/checkSmsPage';
 import GetDataPage from './pages/getDataPage';
 import Hello from './pages/hello';
 
 function LoginScreen() {
-    const { pageType, clientBlank } = useLoginPage()
-
-    const styles = StyleSheet.create({
-        container: {
-            width: "100%",
-            height: "100%",
-            paddingHorizontal: 20,
-            paddingVertical: 20
-        }
-    })
+    const { pageType } = useLoginPage()
 
     return (
-        <SafeAreaView style={styles.container}>
-            {
-                renderPage(pageType)
-            }
-        </SafeAreaView>
+        renderPage(pageType)
     )
 
     function renderPage(pageType: LoginPageType) {
